b4cf94f9510a5df70320bfdb9c4a4413cef315e4
Neel <neel@10-20-126-219.dynapool.wireless.nyu.edu>
2026-09-09 15:04:12 -0400

    Implement Wordguesser game logic and Sinatra routes

--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -1,22 +1,41 @@
 class WordGuesserGame
-  # add the necessary class methods, attributes, etc. here
-  # to make the tests in spec/wordguesser_game_spec.rb pass.
-
-  # Get a word from remote "random word" service
+  attr_accessor :word, :guesses, :wrong_guesses
 
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
 
-  # You can test it by installing irb via $ gem install irb
-  # and then running $ irb -I. -r app.rb
-  # And then in the irb: irb(main):001:0> WordGuesserGame.get_random_word
-  #  => "cooking"   <-- some random word
+  def guess(letter)
+    raise ArgumentError unless letter.is_a?(String) && letter.match?(/\A[a-zA-Z]\z/)
+
+    letter = letter.downcase
+    return false if guesses.include?(letter) || wrong_guesses.include?(letter)
+
+    if word.downcase.include?(letter)
+      self.guesses += letter
+    else
+      self.wrong_guesses += letter
+    end
+    true
+  end
+
+  def word_with_guesses
+    word.chars.map { |letter| guesses.include?(letter.downcase) ? letter : '-' }.join
+  end
+
+  def check_win_or_lose
+    return :win if word.downcase.chars.uniq.all? { |letter| guesses.include?(letter) }
+    return :lose if wrong_guesses.length >= 7
+    :play
+  end
+
   def self.get_random_word
     require 'uri'
     require 'net/http'
-    uri = URI('https://esaas-randomword-27a759b6224d.herokuapp.com/RandomWord') 
-    Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http| 
+    uri = URI('https://esaas-randomword-27a759b6224d.herokuapp.com/RandomWord')
+    Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
       return http.post(uri, "").body
     end
   end
